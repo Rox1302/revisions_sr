3ba0656972b5c5604a3a87ba18028654f8b365f1
Elodie <e.marquez@intech-so.fr>
2017-10-14 16:40:16 +0200

    Ajout des fichiers de bases. Dans le dossier sources et ses sous-dossiers se trouve les fichiers visuels pour les schémas réseau.

---
+++ index.php
@@ -0,0 +1,20 @@
+<?php
+  session_start();
+?>
+
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <link rel="stylesheet" href="css/master.css">
+    <title>Système et réseaux</title>
+  </head>
+
+  <header>
+    menu
+  </header>
+
+  <body>
+    lolilol
+  </body>
+</html>
